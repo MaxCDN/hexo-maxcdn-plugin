35e4ba53eb20d412d4f1348b603ade87dc421179
Josh Mervine <jmervine@yp.com>
2013-11-03 12:50:09 -0800

    Updating integration tests to support shunt (CLIunit refactor).

--- test/integration.sh
+++ test/integration.sh
@@ -20,12 +20,12 @@ function after {
 # tests
 ##
 function run_tests {
-  assert "npm install hexo --silent" "hexo install failed"
-  assert "./node_modules/.bin/hexo init test_blog" "hexo init failed"
-  assert_dir "test_blog" "hexo init failed"
+  assert "npm install hexo --silent" "hexo install"
+  assert "./node_modules/.bin/hexo init test_blog" "hexo init"
+  assert_dir "test_blog" "hexo init"
   cd test_blog
 
-  assert "npm install $WORKING_DIR --silent" "maxcdn plugin install failed"
+  assert "npm install $WORKING_DIR --silent" "maxcdn plugin install"
 
   cp $WORKING_DIR/test/support/after_footer.ejs themes/light/layout/_partial/
   cp $WORKING_DIR/test/support/hello-world.md source/_posts/
@@ -35,46 +35,46 @@ function run_tests {
 
   export NODE_ENV=test
 
-  assert "../node_modules/.bin/hexo generate" "hexo generation failed"
+  assert "../node_modules/.bin/hexo generate" "hexo generation"
 
   unset NODE_ENV
 
   COMMAND="cat $tmp/test_blog/public/2013/10/06/hello-world/index.html"
 
   # helper tests
-  assert_grep "$COMMAND" "<link.*\/\/you\.maxcdn\.com\/helper\.css.*stylesheet" "helper: css failed"
-  assert_grep "$COMMAND" "<link.*\/\/you\.maxcdn\.com\/helper\.ico.*icon" "helper: ico failed"
+  assert_grep "$COMMAND" "<link.*\/\/you\.maxcdn\.com\/helper\.css.*stylesheet" "helper: css"
+  assert_grep "$COMMAND" "<link.*\/\/you\.maxcdn\.com\/helper\.ico.*icon" "helper: ico"
 
-  assert_grep "$COMMAND" "<script.*\/\/you\.maxcdn\.com\/helper\.js.*text\/javascript" "helper: js failed"
+  assert_grep "$COMMAND" "<script.*\/\/you\.maxcdn\.com\/helper\.js.*text\/javascript" "helper: js"
 
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.bmp" "helper: bmp failed"
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.gif" "helper: gif failed"
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.jpg" "helper: jpg failed"
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.jpeg" "helper: jpeg failed"
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.png" "helper: png failed"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.bmp" "helper: bmp"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.gif" "helper: gif"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.jpg" "helper: jpg"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.jpeg" "helper: jpeg"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/helper\.png" "helper: png"
 
-  assert_grep "$COMMAND" "<embed.*\/\/you\.maxcdn\.com\/tag\.pdf" "helper: pdf failed"
-  assert_grep "$COMMAND" "<embed.*\/\/you\.maxcdn\.com\/tag\.svg.*image\/svg" "helper: svg failed"
+  assert_grep "$COMMAND" "<embed.*\/\/you\.maxcdn\.com\/tag\.pdf" "helper: pdf"
+  assert_grep "$COMMAND" "<embed.*\/\/you\.maxcdn\.com\/tag\.svg.*image\/svg" "helper: svg"
 
   # tag tests
-  assert_grep "$COMMAND" "<link.*\/\/you\.maxcdn\.com\/tag\.css.*stylesheet" "tag: css failed"
-  assert_grep "$COMMAND" "<link.*\/\/you\.maxcdn\.com\/tag\.ico.*icon" "tag: ico failed"
+  assert_grep "$COMMAND" "<link.*\/\/you\.maxcdn\.com\/tag\.css.*stylesheet" "tag: css"
+  assert_grep "$COMMAND" "<link.*\/\/you\.maxcdn\.com\/tag\.ico.*icon" "tag: ico"
 
-  assert_grep "$COMMAND" "<script.*\/\/you\.maxcdn\.com\/tag\.js.*text\/javascript" "tag: js failed"
+  assert_grep "$COMMAND" "<script.*\/\/you\.maxcdn\.com\/tag\.js.*text\/javascript" "tag: js"
 
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.bmp" "tag: bmp failed"
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.gif" "tag: gif failed"
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.jpg" "tag: jpg failed"
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.jpeg" "tag: jpeg failed"
-  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.png" "tag: png failed"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.bmp" "tag: bmp"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.gif" "tag: gif"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.jpg" "tag: jpg"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.jpeg" "tag: jpeg"
+  assert_grep "$COMMAND" "<img.*\/\/you\.maxcdn\.com\/tag\.png" "tag: png"
 
-  assert_grep "$COMMAND" "<embed.*\/\/you\.maxcdn\.com\/tag\.pdf" "tag: pdf failed"
-  assert_grep "$COMMAND" "<embed.*\/\/you\.maxcdn\.com\/tag\.svg.*image\/svg" "tag: svg failed"
+  assert_grep "$COMMAND" "<embed.*\/\/you\.maxcdn\.com\/tag\.pdf" "tag: pdf"
+  assert_grep "$COMMAND" "<embed.*\/\/you\.maxcdn\.com\/tag\.svg.*image\/svg" "tag: svg"
 
   # tag tests w/ attrs
-  assert_grep "$COMMAND" "<img.*/tag-with-attrs\.gif.*zfirst='1'" "tag with attrs failed"
-  assert_grep "$COMMAND" "<img.*/tag-with-attrs\.gif.*zsecond='2'" "tag with attrs failed on single quote"
-  assert_grep "$COMMAND" "<img.*/tag-with-attrs\.gif.*zthrid='3'" "tag with attrs failed on double quote"
+  assert_grep "$COMMAND" "<img.*/tag-with-attrs\.gif.*zfirst='1'" "tag with attrs"
+  assert_grep "$COMMAND" "<img.*/tag-with-attrs\.gif.*zsecond='2'" "tag with attrs on single quote"
+  assert_grep "$COMMAND" "<img.*/tag-with-attrs\.gif.*zthrid='3'" "tag with attrs on double quote"
 }
 
 # vim: ft=sh:
